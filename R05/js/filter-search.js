751d072bb63ce4c4d18ee45d6843f4cb7964c7f9
duongpn99 <ngocduong101999@gmail.com>
2023-04-19 09:46:03 +0700

    .
    .

---
+++ R05/js/filter-search.js
@@ -0,0 +1,28 @@
+(function(){
+    var $imgs = $('#gallery img');
+    var #search = $('filter-search');
+    var cache = [];
+
+    $imgs.each(function(){
+        cache.push({
+            Element: this,
+            Text: this.alt.trim().toLowerCase()
+        });
+    });
+    function filter(){
+        var query = this.value.trim().toLowerCase();
+        cache.forEach(function(img){
+            var index = 0;
+
+            if(query){
+                index = img.text.index0f(query);
+            }
+            img.Element.style.display = index === -1 ? 'none' : '';
+        });
+    }
+    if('oninput' in $search[0]){
+        @$search.on('input',filter);
+    } else{
+        $search.on('keyup', filter);
+    }
+}());
